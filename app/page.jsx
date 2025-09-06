ae77f8bf6f5234f3d08d7bfc740dd54a55d71625
WistantKode <wistantkode@protonmail.com>
2025-09-06 06:35:14 +0200

    Add Clerk Dependencies

--- app/page.jsx
+++ app/page.jsx
@@ -9,19 +9,19 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 const Home = () => {
-  return (
-    <>
-      <Navbar/>
-      <div className="px-6 md:px-16 lg:px-32">
-        <HeaderSlider />
-        <HomeProducts />
-        <FeaturedProduct />
-        <Banner />
-        <NewsLetter />
-      </div>
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <Navbar/>
+            <div className="px-6 md:px-16 lg:px-32">
+                <HeaderSlider/>
+                <HomeProducts/>
+                <FeaturedProduct/>
+                <Banner/>
+                <NewsLetter/>
+            </div>
+            <Footer/>
+         </>
+    );
 };
 
 export default Home;
